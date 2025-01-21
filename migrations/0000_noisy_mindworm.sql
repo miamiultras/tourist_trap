1c529e061c543dd2b2ae7f5cb3c6e21a5863b940
eeeeeyyyphophopho <26793642+miamiultras@users.noreply.github.com>
2025-01-21 09:40:41 +0100

    add migration

---
+++ migrations/0000_noisy_mindworm.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "tourist_traps_table" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"location" text NOT NULL,
+	"description" text,
+	"trap_count" integer DEFAULT 0,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now()
+);
